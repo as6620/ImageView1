e883dee4d7adcdbc4a35d2b2c355c8620a72e8f0
as6620 <as6620@bs.amalnet.k12.il>
2024-01-02 09:10:18 +0200

    second

--- app/src/main/java/com/example/imageview1/MainActivity.java
+++ app/src/main/java/com/example/imageview1/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.imageview1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
